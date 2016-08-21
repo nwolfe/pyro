9e33a0b4a25aa328154afa98aef3f60d7db3b3dc
Nate Wolfe <nwolfe@puppet.com>
2016-08-21 02:22:35 -0700

    Finish part 5: Preparing for combat

--- pyro.py
+++ pyro.py
@@ -4,14 +4,35 @@ import map as libmap
 from settings import *
 
 
+class Actions:
+    def __init__(self, fov_recompute=None, player_action=None):
+        self.fov_recompute = fov_recompute
+        self.player_action = player_action
+
+
 class World:
-    def __init__(self, map, player, objects):
+    def __init__(self, map=None, player=None, objects=None, game_state=None):
         self.map = map
         self.player = player
         self.objects = objects
+        self.game_state = game_state
 
-    def move_player(self, dx, dy):
-        self.player.move(self.map, self.objects, dx, dy)
+    def move_player_or_attack(self, dx, dy, actions):
+        x = self.player.x + dx
+        y = self.player.y + dy
+        target = None
+        for object in self.objects:
+            if object.x == x and object.y == y:
+                target = object
+                break
+
+        if target is not None:
+            print 'The {0} laughs at your puny efforts to attack him!'.format(
+                target.name)
+        else:
+            self.player.move(self.map, self.objects, dx, dy)
+            actions.player_action = 'move'
+            actions.fov_recompute = True
 
 
 def handle_keys(world, actions):
@@ -23,27 +44,28 @@ def handle_keys(world, actions):
         libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
     elif key.vk == libtcod.KEY_ESCAPE:
         # Exit game
-        actions['exit'] = True
+        actions.player_action = 'exit'
+        return
+
+    if world.game_state != 'playing':
         return
 
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        world.move_player(0, -1)
-        actions['fov_recompute'] = True
+        world.move_player_or_attack(0, -1, actions)
     elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        world.move_player(0, 1)
-        actions['fov_recompute'] = True
+        world.move_player_or_attack(0, 1, actions)
     elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-        world.move_player(-1, 0)
-        actions['fov_recompute'] = True
+        world.move_player_or_attack(-1, 0, actions)
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        world.move_player(1, 0)
-        actions['fov_recompute'] = True
+        world.move_player_or_attack(1, 0, actions)
+    else:
+        actions.player_action = 'idle'
 
 
 def render_all(con, world, fov_map, actions):
-    if 'fov_recompute' in actions:
+    if actions.fov_recompute:
         # Recompute FOV if needed (i.e. the player moved)
-        actions['fov_recompute'] = False
+        actions.fov_recompute = False
         libtcod.map_compute_fov(fov_map, player.x, player.y,
                                 TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO)
 
@@ -82,7 +104,7 @@ con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 player = libobj.Object(0, 0, '@', 'player', libtcod.white, blocks=True)
 objects = [player]
 map = libmap.make_map(player, objects)
-world = World(map, player, objects)
+world = World(map=map, player=player, objects=objects, game_state='playing')
 
 fov_map = libtcod.map_new(MAP_WIDTH, MAP_HEIGHT)
 for y in range(MAP_HEIGHT):
@@ -91,7 +113,7 @@ for y in range(MAP_HEIGHT):
                                    not world.map[x][y].block_sight,
                                    not world.map[x][y].blocked)
 
-actions = dict(fov_recompute = True)
+actions = Actions(fov_recompute=True)
 
 while not libtcod.console_is_window_closed():
     render_all(con, world, fov_map, actions)
@@ -102,5 +124,12 @@ while not libtcod.console_is_window_closed():
         object.clear(con)
 
     handle_keys(world, actions)
-    if 'exit' in actions:
+
+    if actions.player_action == 'exit':
         break
+
+    if world.game_state == 'playing' and actions.player_action != 'idle':
+        for object in world.objects:
+            if object != world.player:
+                print 'The {0} growls!'.format(object.name)
+
