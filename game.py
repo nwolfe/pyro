852404ed720738716869a3dc4cd62b32853f405d
Nate Wolfe <nwolfe@puppet.com>
2016-09-25 22:53:20 -0700

    Inline top-level message function into Game class

--- game.py
+++ game.py
@@ -4,18 +4,6 @@ import fighter as libfighter
 import textwrap
 from settings import *
 
-def message(messages, new_msg, color=libtcod.white):
-    # Split the message if necessary, among multiple lines
-    new_msg_lines = textwrap.wrap(new_msg, MSG_WIDTH)
-
-    for line in new_msg_lines:
-        # If the buffer is full, remove the first line to make room
-        if len(messages) == MSG_HEIGHT:
-            del messages[0]
-
-        # Add the new line as a tuple, with the text and color
-        messages.append((line, color))
-
 
 class Game:
     def __init__(self,
@@ -43,7 +31,16 @@ class Game:
         self.dungeon_level = dungeon_level
 
     def message(self, text, color=libtcod.white):
-        message(self.messages, text, color)
+        # Split the message if necessary, among multiple lines
+        new_msg_lines = textwrap.wrap(text, MSG_WIDTH)
+
+        for line in new_msg_lines:
+            # If the buffer is full, remove the first line to make room
+            if len(self.messages) == MSG_HEIGHT:
+                del self.messages[0]
+
+            # Add the new line as a tuple, with the text and color
+            self.messages.append((line, color))
 
     def target_tile(self, ui, max_range=None):
         # Return the position of a tile left-clicked in player's FOV
