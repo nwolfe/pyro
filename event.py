b23976d3c8126177c4784dc2b21ac68ab3dda0e9
Nate Wolfe <nwolfe@puppet.com>
2016-10-05 00:58:45 -0700

    EventHandlers have a priority to allow for sorting

--- event.py
+++ event.py
@@ -1,6 +1,9 @@
 
 
 class EventHandler:
+    def __init__(self, priority=100):
+        self.priority = priority
+
     def handle_event(self, event, data):
         return
 
@@ -9,17 +12,18 @@ class EventEmitter:
     def __init__(self):
         self.event_handlers = dict()
 
-    def fire_event(event, data=None):
+    def fire_event(self, event, data=None):
         if self.event_handlers.has_key(event):
             if data is None:
                 data = dict()
             for handler in self.event_handlers[event]:
                 handler.handle_event(event, data)
 
-    def add_event_handler(handler, *events):
+    def add_event_handler(self, handler, *events):
         for event in events:
             if self.event_handlers.has_key(event):
                 self.event_handlers[event].append(handler)
             else:
-                handlers = list(handler)
+                handlers = [handler]
                 self.event_handlers[event] = handlers
+            self.event_handlers[event].sort(key=lambda h: h.priority)
