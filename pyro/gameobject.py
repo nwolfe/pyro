8886cc00b584b73792633786c260d9eeb998d820
Nate Wolfe <a1979shakedown@gmail.com>
2017-04-04 23:38:21 -0700

    Extract component behavior from GameObject into new Entity class

--- pyro/gameobject.py
+++ pyro/gameobject.py
@@ -1,26 +1,11 @@
+from pyro.entity import Entity
 from pyro.position import Position
 
 
-class GameObject:
+class GameObject(Entity):
     def __init__(self, name=None, blocks=False, components=None, game=None):
+        self.game = game
         self.pos = Position()
         self.name = name
         self.blocks = blocks
-        self.game = game
-
-        self.components = {}
-        if components:
-            for comp in components:
-                self.set_component(comp)
-
-    def component(self, component_type):
-        return self.components.get(component_type)
-
-    def set_component(self, component):
-        self.remove_component(component.type)
-        self.components[component.type] = component
-        component.set_owner(self)
-
-    def remove_component(self, component_type):
-        if component_type in self.components:
-            self.components.pop(component_type)
+        Entity.__init__(self, components)
