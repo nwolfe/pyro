416111f62bdc4e3d0b4bbc2683351115e0f2cbf5
Nate Wolfe <a1979shakedown@gmail.com>
2017-04-26 23:26:35 -0700

    Remove render_order from Graphics component

--- pyro/components/graphics.py
+++ pyro/components/graphics.py
@@ -1,10 +1,8 @@
 from pyro.component import Component
-from pyro.settings import RENDER_ORDER_DEFAULT
 
 
 class Graphics(Component):
-    def __init__(self, glyph=None, color=None, render_order=RENDER_ORDER_DEFAULT):
+    def __init__(self, glyph=None, color=None):
         Component.__init__(self, component_type=Graphics)
         self.glyph = glyph
         self.color = color
-        self.render_order = render_order
