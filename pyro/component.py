04d7361c01f4d2bd026a78e44b1eb493f5985b0f
Nate Wolfe <a1979shakedown@gmail.com>
2017-04-29 00:37:57 -0700

    Delete Entity and Component classes now that Actor-Action is implemented

--- pyro/component.py
+++
@@ -1,9 +0,0 @@
-
-
-class Component:
-    def __init__(self, component_type=None):
-        self.type = component_type
-        self.owner = None
-
-    def set_owner(self, game_object):
-        self.owner = game_object
