fc91c29a06cbdebdd28f7fb8c4182fa87158c4ad
Nate Wolfe <a1979shakedown@gmail.com>
2018-02-21 20:33:08 -0800

    Extract ActorEffect parent class

--- pyro/ui/effects.py
+++ pyro/ui/effects.py
@@ -14,46 +14,66 @@ class Effect:
         pass
 
 
-class HitEffect(Effect):
+class ActorEffect(Effect):
+    """Renders a colored character over the top of the actor."""
+    __metaclass__ = abc.ABCMeta
+
     def __init__(self, actor):
-        Effect.__init__(self)
         self.actor = actor
-        self.frame = 0
-
-    def update(self, game):
-        self.frame += 1
-        return self.frame < 4
-
-    def render(self, game, ui):
-        x, y = self.actor.pos.x, self.actor.pos.y
-        libtcod.console_set_default_foreground(ui.console, libtcod.dark_red)
-        libtcod.console_put_char(ui.console, x, y, '*', libtcod.BKGND_NONE)
-
-
-class HealEffect(Effect):
-    def __init__(self, actor):
-        Effect.__init__(self)
-        self._actor = actor
         self._frame = 0
 
     def update(self, game):
         self._frame += 1
-        return self._frame < 8
+        return self._frame < self.num_frames()
 
     def render(self, game, ui):
-        # Alternate between white and green for a cool visual effect
-        if self._frame <= 2:
-            color = libtcod.white
-        elif self._frame <= 4:
-            color = libtcod.green
-        elif self._frame <= 6:
-            color = libtcod.white
-        else:
-            color = libtcod.green
-
-        x, y = self._actor.pos.x, self._actor.pos.y
+        char = self.char(self._frame)
+        color = self.color(self._frame)
+        x, y = self.actor.pos.x, self.actor.pos.y
         libtcod.console_set_default_foreground(ui.console, color)
-        libtcod.console_put_char(ui.console, x, y, '+', libtcod.BKGND_NONE)
+        libtcod.console_put_char(ui.console, x, y, char, libtcod.BKGND_NONE)
+
+    @abc.abstractmethod
+    def num_frames(self):
+        pass
+
+    @abc.abstractmethod
+    def char(self, frame):
+        pass
+
+    @abc.abstractmethod
+    def color(self, frame):
+        pass
+
+
+class HitEffect(ActorEffect):
+    def num_frames(self):
+        return 4
+
+    def char(self, frame):
+        return '*'
+
+    def color(self, frame):
+        return libtcod.dark_red
+
+
+class HealEffect(ActorEffect):
+    def num_frames(self):
+        return 8
+
+    def char(self, frame):
+        return '+'
+
+    def color(self, frame):
+        # Alternate between white and green for a cool visual effect
+        if frame <= 2:
+            return libtcod.white
+        elif frame <= 4:
+            return libtcod.green
+        elif frame <= 6:
+            return libtcod.white
+        else:
+            return libtcod.green
 
 
 class BoltEffect(Effect):
